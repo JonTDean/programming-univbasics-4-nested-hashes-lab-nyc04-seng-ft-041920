b8d7ac3a54afd4f982dbea889b9aee54ab2d87b6
Jonathan T Dean <jonathantomdean@gmail.com>
2020-04-23 16:01:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -76,12 +76,12 @@ def adding_matz
         :known_for => "Unix",
         :languages => ["C"]
       },
-      :yukihiro_matsumoto => {
-      :known_for => "Ruby",
-      :languages => ["LISP", "C"]
-      }
     }
     
+    myHash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
   programmer_hash
 end
 
